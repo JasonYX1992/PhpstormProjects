fa4f2ebf547afe0dc60b3ee89cd1dbb4163162ea
jasonyangxing <xing.yang@my.jcu.edu.au>
2017-05-09 12:38:52 +0800

    Modify some issue

--- LearningPrac.js
+++ LearningPrac.js
@@ -4,7 +4,7 @@
 function selectionSort (arr) {
     var len = arr.length;
     var minIndex, temp;
-    for (var i = 0,i<len-1;i++){
+    for (var i = 0;i< len-1;i++){
         minIndex=i;
         for(var j=i+1;j<len;j++){
             if(arr[j]<arr[minIndex]){
